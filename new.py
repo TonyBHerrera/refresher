c5d2ae24b0957ca0ac26ad44f7f8a600317a14d0
TonyBHerrera <tonybherrera@yahoo.com>
2020-06-10 13:32:26 -0600

    remembering loops

--- new.py
+++ new.py
@@ -1,15 +1,31 @@
-def sum(num_one, num_two):
-    print(num_one + num_two)
+# def sum(num_one, num_two):
+#     print(num_one + num_two)
 
-sum(2,4)
+# sum(2,4)
 
-def hello_world():
-    print("Hello World")
+# def hello_world():
+#     print("Hello World")
 
-hello_world()
+# hello_world()
 
-post_count = 42
-name = 'Kristine'
+# post_count = 42
+# name = 'Kristine'
 
-print(name)
-print(post_count)
+# print(name)
+# print(post_count)
+
+# players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+
+# for player in players:
+#     print(player)
+
+players = {
+    '2b': "Altuve",
+    '3b': 'Bregman',
+    'ss': 'Bregman',
+    'dh': 'Gattis'
+}
+
+for position, player in players.items():
+    print('Position', position)
+    print('Player', player)
