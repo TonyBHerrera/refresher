dafa2b59342ff41ae0248b62e0ce7e14f2917553
TonyBHerrera <tonybherrera@yahoo.com>
2020-06-10 13:12:57 -0600

    inital commit

---
+++ new.py
@@ -0,0 +1,15 @@
+def sum(num_one, num_two):
+    print(num_one + num_two)
+
+sum(2,4)
+
+def hello_world():
+    print("Hello World")
+
+hello_world()
+
+post_count = 42
+name = 'Kristine'
+
+print(name)
+print(post_count)
